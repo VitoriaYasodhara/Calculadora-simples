bab21fed8a0c57aee26b70dcfe4292180e938fc5
VitoriaYasodhara <viviyasodhara@gmail.com>
2025-09-18 09:41:39 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
 <!--CDN Bootstrap-->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">  
- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+ 
  
 <!--Fonte Letra-->
 <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -19,6 +19,7 @@
 
 </head>
 <body>
+  
   <div class="container">
     <h1>Calculadora</h1>
     <div id="calculator">
@@ -26,6 +27,27 @@
         <div id="display">
           <div id="results">
             Resultado:
+            <!--PHP-->
+
+            <?php
+              if(isset($_POST['acao'])):
+                $n1 = str_replace(',','.',$_POST['n1']);
+                $n2 = str_replace(',','.',$_POST['n2']);
+                $tipo = $_POST['tipo'];
+
+                  if($tipo=='+'):
+                    $calcular = $n1 + $n2;
+                  elseif($tipo == '-'):
+                    $calcular = $n1 - $n2;
+                  elseif($tipo == '/'):
+                    $calcular = $n1 / $n2;
+                  else:
+                    $calcular = $n1 * $n2;
+                  endif;
+                  $calcular = str_replace('.',',', $calcular);
+                  echo "$n1 $tipo $n2 igual a $calcular";
+                endif;
+            ?>
           </div>
         </div>
         <div id="keyboard">
@@ -55,7 +77,6 @@
   </div>
   <!--CDN Bootstrap-->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">  
 
 </body>
 </html>
